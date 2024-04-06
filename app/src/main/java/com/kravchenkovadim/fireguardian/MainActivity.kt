517f993fa89b6b50f0493c7acab5c302256a1df6
Arhizmei <arhizmei@gmail.com>
2024-04-06 23:40:57 +0300

    experiment

--- app/src/main/java/com/kravchenkovadim/fireguardian/MainActivity.kt
+++ app/src/main/java/com/kravchenkovadim/fireguardian/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.kravchenkovadim.fireguardian.ui.theme.FireGuardianTheme
-
+//experiment
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
