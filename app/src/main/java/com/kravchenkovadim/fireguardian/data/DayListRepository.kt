996d2d2c2b8b87a770bc07d4b53edfabb7ab5b4f
job <arhizmei@gmail.com>
2024-04-07 16:28:18 +0300

    add Repo DayList

--- app/src/main/java/com/kravchenkovadim/fireguardian/data/DayListRepository.kt
+++ app/src/main/java/com/kravchenkovadim/fireguardian/data/DayListRepository.kt
@@ -1,17 +1,16 @@
 package com.kravchenkovadim.fireguardian.data
 
-import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
-@Dao
-interface DayListDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+
+interface DayListRepository {
+
     suspend fun insertTask(task: DayListTasks)
-    @Delete
+
     suspend fun deleteTask(task: DayListTasks)
-    @Query("SELECT * FROM day_list_tasks")
+
     fun getAllTasks(): Flow<List<DayListTasks>>
 }
