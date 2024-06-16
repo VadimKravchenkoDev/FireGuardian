4cdec780729f6eac5b604150946605bdc3cac847
kravhcneko.vadim <kravhcneko.vadim.dev@gmail.com>
2024-06-16 21:57:07 +0300

    rename aplicationId in build.gradle.kts module app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.kravchenkovadim.shoppinglist"
+        applicationId = "com.kravchenkovadim.fireguardian"
         minSdk = 23
         targetSdk = 34
         versionCode = 1
