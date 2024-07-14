a385bfc594b1822d5fe0e3c2235791b382cff828
kravchenko.vadim <kravhcneko.vadim.dev@gmail.com>
2024-07-14 13:58:16 +0300

    change position and picture of the floating button

--- app/src/main/java/com/kravchenkovadim/fireguardian/main_screen/MainScreen.kt
+++ app/src/main/java/com/kravchenkovadim/fireguardian/main_screen/MainScreen.kt
@@ -1,22 +1,31 @@
 package com.kravchenkovadim.fireguardian.main_screen
 
 import android.annotation.SuppressLint
+import androidx.compose.material.FabPosition
 import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import com.kravchenkovadim.fireguardian.R
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
-    Scaffold(bottomBar = {
-        
-    },
+    Scaffold(
+        bottomBar = {
+
+        },
         floatingActionButton = {
             FloatingActionButton(onClick = { /*TODO*/ }) {
-                
+                Icon(
+                    painter = painterResource(id = R.drawable.add_icon),
+                    contentDescription = "Add"
+                )
             }
-        }
-        ) {
+        },
+        floatingActionButtonPosition = FabPosition.Center
+    ) {
 
     }
 }
