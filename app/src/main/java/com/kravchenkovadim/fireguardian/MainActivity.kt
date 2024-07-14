be93fa0c212213e3123da0d613dade08575bf0e9
kravhcneko.vadim <kravhcneko.vadim.dev@gmail.com>
2024-07-14 13:49:02 +0300

    add Main_screen, Scaffold, floatingActionButton

--- app/src/main/java/com/kravchenkovadim/fireguardian/MainActivity.kt
+++ app/src/main/java/com/kravchenkovadim/fireguardian/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.kravchenkovadim.fireguardian.main_screen.MainScreen
 import com.kravchenkovadim.fireguardian.ui.theme.FireGuardianTheme
 import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
@@ -23,25 +24,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
     }
 }
-//experiment
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hi $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FireGuardianTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/kravchenkovadim/fireguardian/main_screen/MainScreen.kt
@@ -0,0 +1,22 @@
+package com.kravchenkovadim.fireguardian.main_screen
+
+import android.annotation.SuppressLint
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MainScreen() {
+    Scaffold(bottomBar = {
+        
+    },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                
+            }
+        }
+        ) {
+
+    }
+}
