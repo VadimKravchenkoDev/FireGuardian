9fa0fbb2726ef28cca4820a204c4ff631a70279d
Arhizmei <arhizmei@gmail.com>
2025-06-11 16:26:03 +0300

    11/06/2025

--- app/src/main/java/com/kravchenkovadim/fireguardian/MainActivity.kt
+++ app/src/main/java/com/kravchenkovadim/fireguardian/MainActivity.kt
@@ -14,7 +14,8 @@ import com.kravchenkovadim.fireguardian.main_screen.MainScreen
 import com.kravchenkovadim.fireguardian.ui.theme.FireGuardianTheme
 import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class .
+MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
